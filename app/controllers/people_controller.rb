50fce9afabc57cc4c196ff8a4213f550929b6cbc
Rick Conrad <richardjconrad@gmail.com>
2017-07-05 08:14:22 +0100

    Start to refactor person form

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,7 +9,8 @@ class PeopleController < ApplicationController
   end
 
   def create
-    Person.create last_name: params[:last_name], first_name: params[:first_name], category: params[:category], job: params[:job], arrival: params[:arrival], departure: params[:departure], nationality: params[:nationality], title: params[:title], gender: params[:gender], language_id: params[:language]
+    Person.create params[:person] 
+    # last_name: params[:last_name], first_name: params[:first_name], category: params[:category], job: params[:job], arrival: params[:arrival], departure: params[:departure], nationality: params[:nationality], title: params[:title], gender: params[:gender], language_id: params[:language]
     redirect_to people_path
     puts params
   end
