cd317977c82e4292f9097ec26d77a6ff5d2b42a9
Brian Yee <brian_yee@sil.org>
2018-01-17 15:36:54 +0100

    Various Styling Updates
    * Add bootstrap to gemlist and imports into application.scss

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,12 +5,12 @@ class LanguagesController < ApplicationController
   end
 
   def new
-    @regions = Region.order(:name)
+    @regions = Region.all
   end
 
   def create
    Language.create name: params[:name], region_id: params[:region]
    redirect_to languages_path
-  end 
-  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "dashboard#dash"
 
-  resources :people
+  resources :people do
+    resources :leave
+  end
+
+  resources :leave, only: ["index"]
+
   post 'people/:person_id/attach_language', to: 'people#attach', as: :attach_language
 
   resources :languages
