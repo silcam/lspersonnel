d44ccaab31ee15d2bf2adaf7866656f4266408ed
Brian Yee <brian_yee@sil.org>
2018-02-09 10:57:53 +0100

    Additional Views Updates Languages/Departments
    Departments can be selected per language via a
    Wall of Checkboxes.  Handle appropriately in
    controller and add controller tests.
    
    Documents can be generated from the resulting
    data.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,16 +5,50 @@ class LanguagesController < ApplicationController
   end
 
   def new
+    @language = Language.new
     @regions = Region.all
   end
 
   def edit
+    @language = Language.find(params[:id])
+
     @regions = Region.all
+    @departments = Department.all.order(:region_id)
   end
 
   def create
-   Language.create name: params[:name], region_id: params[:region]
-   redirect_to languages_path
+    @language = Language.new(language_params)
+
+    if (@language.valid?)
+      @language.save
+      redirect_to languages_path()
+    else
+      @regions = Region.all
+      @departments = Department.all.order(:region_id)
+      render 'new'
+    end
+  end
+
+  def update
+    @language = Language.find(params[:id])
+
+    if @language.update language_params
+      redirect_to languages_path()
+    else
+      @regions = Region.all
+      @departments = Department.all.order(:region_id)
+      render 'new'
+    end
+  end
+
+  private
+
+  def language_params
+    permitted = [
+      :name,
+      department_ids: []
+    ]
+    params.require(:language).permit(permitted)
   end
 
 end
--- test/controllers/languages_controller_test.rb
+++ test/controllers/languages_controller_test.rb
@@ -1,9 +1,41 @@
 require 'test_helper'
 
 class LanguagesControllerTest < ActionDispatch::IntegrationTest
-#  test "should get index" do
-#    get languages_index_url
-#    assert_response :success
-#  end
+
+  def setup
+    do_login
+  end
+
+  test "index" do
+    get languages_url()
+
+    assert_response :success
+  end
+
+  test "new form" do
+    get new_language_url()
+
+    assert_response :success
+  end
+
+  test "edit form" do
+    @language = languages :Sso
+    get edit_language_url(@language)
+
+    assert_response :success
+  end
+
+  test "update language" do
+    @language = languages :Sso
+
+    patch language_url(@language), params: {
+      language: { name: "Swo" }
+    }
+
+    assert_redirected_to languages_path()
+    follow_redirect!
+
+    assert(@response.body.index("Swo") > 0, "contains new name")
+  end
 
 end
